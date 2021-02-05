5c37c8f8e14e677c16959663a21a6de3c18e3146
Mxl <hcws258@163.com>
2021-02-05 23:03:09 +0800

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    这是一个很衰的博客
                 </div>
 
                 <div class="links">
